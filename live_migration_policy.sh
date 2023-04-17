a85c00b19f8f6fdb9f1daf72920d1f932c7e6513
Maor Roizman Gheiler <maor.roizman@utec.edu.pe>
2023-04-16 23:31:08 -0500

    Create live_migration_policy.sh

---
+++ live_migration_policy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Set the maximum CPU usage and memory usage thresholds
+cpu_threshold=80
+memory_threshold=75
+
+# Set the destination host for the live migration
+destination_host=192.168.1.100
+
+# Set the name of the virtual machine to monitor and migrate
+vm_name=my_vm
+
+while true
+do
+  # Get the current CPU usage percentage for the VM
+  cpu_usage=$(VBoxManage metrics $vm_name get CPU/Load/User|awk '{print $NF}'|cut -d'.' -f1)
+  
+  # Get the current memory usage percentage for the VM
+  memory_usage=$(VBoxManage metrics $vm_name get Memory/Usage/Used|awk '{print $NF}'|cut -d'.' -f1)
+  
+  # Check if the CPU usage or memory usage exceeds the threshold
+  if [ "$cpu_usage" -gt "$cpu_threshold" ] && [ "$memory_usage" -gt "$memory_threshold" ]
+  then
+    echo "CPU usage for $vm_name is above the threshold of $cpu_threshold% and memory usage is above the threshold of $memory_threshold% - triggering live migration..."
+    
+    # Initiate a live migration to the specified host
+    VBoxManage controlvm $vm_name migrate --live $destination_host
+    
+    echo "Live migration completed."
+  fi
+  
+  # Sleep for 10 seconds before checking again
+  sleep 10
+done &
